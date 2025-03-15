17f1b63cc887b61a5c446474def86ea62fc92db7
Scrooman <kubcwiek@gmail.com>
2025-03-15 12:36:55 +0100

    1

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Zmienne dla opcji menu
     const menuOptions = document.querySelectorAll('.menu_option_label_container');
     const contentIframe = document.getElementById('contentIframe');
+    
+        // Zmienne dla sekcji typu moba
+    const monsterTypeContainers = document.querySelectorAll('.monster_type_label .monster_type_container');
+    const spawnLvlLabel = document.getElementById('spawnLvlLabel');
 
+    // Dodaj nasłuchiwanie na kliknięcia opcji menu
     menuOptions.forEach(option => {
-        option.addEventListener('click', function() {
-            const optionText = this.textContent.trim();
-            loadIframeContent(optionText);
-        });
+        option.addEventListener('click', handleMenuOptionClick);
     });
 
+    // Funkcja do obsługi kliknięcia opcji menu
+    function handleMenuOptionClick() {
+        const optionText = this.textContent.trim();
+        loadIframeContent(optionText);
+    }
+
+    // Funkcja do wyświetlania odpowiedniej zawartości w iframe
     function loadIframeContent(option) {
         let contentUrl = '';
         switch(option) {
             case 'Character':
                 contentUrl = 'character.html';
                 break;
-        }
+        } // do dodania kolejne warunki dla innych opcji
 
         contentIframe.src = contentUrl;
     }
+
+
+    // Funkcja do wyświetlania etykiety poziomu spawn
+    function showSpawnLevelLabel() {
+            spawnLvlLabel.style.display = 'block'; // Displays the section
+    }
+
+    
+    // Dodaj nasłuchiwanie na kliknięcia kontenerów typu potwora
+    monsterTypeContainers.forEach(container => {
+        container.addEventListener('click', showSpawnLevelLabel);
+    });
+
+    // Ukryj sekcję na początku
+    spawnLvlLabel.style.display = 'none';
+
 });
