60559712be783291d4ecb1ba3c70be32d7427371
Scrooman <kubcwiek@gmail.com>
2025-03-17 06:46:46 +0100

    1

--- character_script.js
+++ character_script.js
@@ -21,14 +21,14 @@ document.addEventListener('DOMContentLoaded', function() {
             elementId: 'className',
             valuePath: [],
             transform: (character) => {
-            let className = character.class;
-            let raceName = character.race
-            if (raceName === 4) {
+                let className = character.class;
+                let raceName = character.race;
+                if (raceName === 4) {
                 raceName = "Human";
-            } else if (className === 8) {
+                } else if (className === 8) {
                 className = "Knight";
-            }
-            return `${character.race} ${className}`;
+                }
+                return `${character.race} ${className}`;
             }
             },
             {
