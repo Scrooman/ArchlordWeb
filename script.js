c8f7171ac9310b4986c1f44f813134e5f4f92d1e
Scrooman <kubcwiek@gmail.com>
2025-03-18 08:04:54 +0100

    1

--- script.js
+++ script.js
@@ -142,8 +142,19 @@ document.addEventListener('DOMContentLoaded', function() {
     function showSpawnLevelLabel(event) {
         const clickedElement = event.currentTarget;
         const mobType = clickedElement.id.split('_').pop(); // Extract mob type from the ID after the last "_"
-        spawnLvlLabel.style.display = 'flex'; // Displays the section
-        updateSpawnList(mobType); // Updates the spawn list with the extracted mob type
+        
+        if (spawnLvlLabel.style.display === 'flex') {
+            // If the section is already displayed, reload it
+            spawnLvlLabel.style.display = 'none'; // Temporarily hide the section
+            setTimeout(() => {
+                spawnLvlLabel.style.display = 'flex'; // Redisplay the section
+                updateSpawnList(mobType); // Updates the spawn list with the extracted mob type
+            }, 0); // Small delay to ensure proper reloading
+        } else {
+            // If the section is not displayed, show it and update
+            spawnLvlLabel.style.display = 'flex'; // Displays the section
+            updateSpawnList(mobType); // Updates the spawn list with the extracted mob type
+        }
     }
 
     // Dodaj nasłuchiwanie na kliknięcia kontenerów typu potwora
