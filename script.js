72eeb794c408560adeefe1c52e443a91ea8aecf7
Scrooman <kubcwiek@gmail.com>
2025-03-21 17:03:06 +0100

    1

--- script.js
+++ script.js
@@ -251,7 +251,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     spawnLvlChangeButtonHigher.addEventListener('click', showHigherLvlSpawn);
 
-    function activateSpawnForSpawnLvl(chosedSpawnLvlButtonName) {
+    function activateSpawnForSpawnLvl(chosedSpawnLvlButtonName, mobType) {
         let mobSpawnLevel;
         switch (chosedSpawnLvlButtonName) {
             case 'center':
@@ -271,9 +271,9 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('No spawn level selected to activate.');
             return;
         }
-        console.log('Activating spawn level:', mobSpawnLevel);
+        console.log('Activating spawn level:', mobSpawnLevel, 'for mob type:', mobType); // Added mobType to the log
         const url = `http://127.0.0.1:5000/set_active_spawn_for_spawn_lvl`;
-        const payload = { spawnLevel: mobSpawnLevel };
+        const payload = { spawnLevel: mobSpawnLevel, mobType: mobType }; // Added mobType to the payload
 
         fetch(url, {
             method: 'POST',
